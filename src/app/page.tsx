58a6749906b8dfe0fa7604203a03055c33616ee2
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2026-09-02 16:19:44 +0800

    feat: add navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div className="w-full flex justify-center items-center">
-          <Image
-            src="/lscs-logo.png"
-            alt="LSCS logo"
-            width={180}
-            height={38}
-            priority
-          />
-        </div>
-        <h1 className="text-4xl font-bold tracking-tight">san-kain</h1>
-        <p className="text-gray-500 text-center sm:text-left">
-          A LSCS project
-        </p>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <span className="text-sm text-gray-500">
-          &copy; {new Date().getFullYear()} LSCS
-        </span>
-      </footer>
-    </div>
-  );
+  return <div className="min-h-[calc(100vh-4rem)]" />;
 }
---
+++ src/components/organisms/Navbar.tsx
@@ -0,0 +1,14 @@
+export const Navbar = () => {
+  return (
+    <nav className="flex items-center justify-between h-20 bg-emerald-600 px-6 py-[18px]">
+      <div className="flex flex-col gap-1">
+        <span className="text-white text-2xl sm:text-3xl font-bold tracking-tight">
+          San Kain
+        </span>
+        <span className="text-white text-sm sm:text-base font-medium tracking-wide">
+          San Tayo Kakain
+        </span>
+      </div>
+    </nav>
+  );
+};
