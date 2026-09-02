883601e5a045984e02db642cb84582c9e8d71942
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2026-09-02 15:45:11 +0800

    feat: setup placeholder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,40 +13,14 @@ export default function Home() {
             priority
           />
         </div>
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>.
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://github.com/dlsu-lscs"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Visit LSCS
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Next.js Docs
-          </a>
-        </div>
+        <h1 className="text-4xl font-bold tracking-tight">san-kain</h1>
+        <p className="text-gray-500 text-center sm:text-left">
+          A LSCS project
+        </p>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <span className="text-sm text-gray-500">
-          © 2025 LSCS App
+          &copy; {new Date().getFullYear()} LSCS
         </span>
       </footer>
     </div>
